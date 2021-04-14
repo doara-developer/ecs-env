a3b3822515985bf2b87c722e392ec5ab69bd6844
doara-developer <doaradev@gmail.com>
2021-04-14 20:35:30 +0900

    Add subnet in vpc

--- main.tf
+++ main.tf
@@ -24,3 +24,72 @@ resource "aws_vpc" "vpc" {
     Name = "ecs-env"
   }
 }
+
+resource "aws_subnet" "public_1a" {
+  vpc_id = aws_vpc.vpc.id
+
+  availability_zone = "ap-northeast-1a"
+
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "ecs-env-public-1a"
+  }
+}
+resource "aws_subnet" "public_1c" {
+  vpc_id = aws_vpc.vpc.id
+
+  availability_zone = "ap-northeast-1c"
+
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "ecs-env-public-1c"
+  }
+}
+
+resource "aws_subnet" "public_1d" {
+  vpc_id = aws_vpc.vpc.id
+
+  availability_zone = "ap-northeast-1d"
+
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "ecs-env-public-1d"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private_1a" {
+  vpc_id = aws_vpc.vpc.id
+
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.10.0/24"
+
+  tags = {
+    Name = "ecs-env-private-1a"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id = aws_vpc.vpc.id
+
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.20.0/24"
+
+  tags = {
+    Name = "ecs-env-private-1c"
+  }
+}
+
+resource "aws_subnet" "private_1d" {
+  vpc_id = aws_vpc.vpc.id
+
+  availability_zone = "ap-northeast-1d"
+  cidr_block        = "10.0.30.0/24"
+
+  tags = {
+    Name = "ecs-env-private-1d"
+  }
+}
